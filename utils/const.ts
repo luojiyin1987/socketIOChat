10cf78796c6f3415b85ad52ec1ceb7b4dc56e0ad
luojiyin <luojiyin@hotmail.com>
2020-04-28 11:54:43 +0800

    config: seal use and salt password

---
+++ utils/const.ts
@@ -0,0 +1,9 @@
+export const SealText = '你已经被关进小黑屋中, 请反思后再试';
+
+export const SealUserTimeout = 1000 * 60 * 10; // 10分钟
+
+export const SealIpTimeout = 1000 * 60 * 60 * 6; // 6小时
+
+export const transparentImage = 'data:image/png;base64,R0lGODlhFAAUAIAAAP///wAAACH5BAEAAAAALAAAAAAUABQAAAIRhI+py+0Po5y02ouz3rz7rxUAOw==';
+
+export const saltRounds = 10;
